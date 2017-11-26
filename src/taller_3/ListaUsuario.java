cab38dfea36814410b659e111b560229e8d6bcc0
YeisonOlivares <33041544+YeisonOlivares@users.noreply.github.com>
2017-11-26 17:33:55 -0300

    Add files via upload

--- src/taller_3/ListaUsuario.java
+++ src/taller_3/ListaUsuario.java
@@ -11,4 +11,59 @@ package taller_3;
  */
 public class ListaUsuario {
     
+    private NodoSimple head;
+    private int cantUsuarios;
+    
+    public ListaUsuario(){
+        this.head = null;
+        this.cantUsuarios = 0;
+    }
+    
+    public boolean agregarUsuario(Usuario usuario){
+        NodoSimple nuevoNodo = new NodoSimple(usuario);
+        NodoSimple aux = this.head;
+        if(this.head == null){
+            this.head = nuevoNodo;
+            this.cantUsuarios++;
+            return true;
+        }else{
+           while(aux.getNext()!= null){
+               if(aux.getUsuario().getCorreo().equals(usuario.getCorreo())){
+                   return false;
+               }else{
+                  aux = aux.getNext();  
+               }
+           }
+        }
+        
+        aux.setNext(nuevoNodo);
+        this.cantUsuarios++;
+        return true;      
+    }
+    
+    public boolean eliminarUsuario(String correo){
+        if(this.head == null){
+            return false;
+        }
+        if(this.head.getUsuario().getCorreo().equals(correo)){
+            this.head = this.head.getNext();
+            this.cantUsuarios--;
+            return true;
+        }
+        NodoSimple aux = this.head;
+        NodoSimple anterior = this.head;
+        
+        while(aux.getNext() != null){
+            if(aux.getUsuario().getCorreo().equals(correo)){
+                anterior.setNext(aux.getNext());
+                this.cantUsuarios--;
+                return true;
+            }else{
+                anterior = aux;
+                aux = aux.getNext();
+            }
+        }
+        return false;
+    }
+    
 }
--- src/taller_3/NodoSimple.java
+++ src/taller_3/NodoSimple.java
@@ -19,65 +19,16 @@ public class NodoSimple {
         this.next = null;
     }
     
-    public String getNombres(){
-        return this.usuario.getNombres();
+    public Usuario getUsuario(){
+        return this.usuario;
     }
     
-    public String getApPaterno(){
-        return this.usuario.getApPaterno();
+    public NodoSimple getNext(){
+        return this.next;
     }
     
-    public String getApMaterno(){
-        return this.usuario.getApMaterno();
+    public void setNext(NodoSimple usuario){
+        this.next = usuario;
     }
-    
-    public String getCorreo(){
-        return this.usuario.getCorreo();
-    }
-    
-    public String getPassword(){
-        return this.usuario.getPassword();
-    }
-    
-    public String getFechaNacimiento(){
-        return this.usuario.getFechaNacimiento();
-    }
-    
-    public char getSexo(){
-        return this.usuario.getSexo();
-    }
-    
-    public ListaUsuario getListaAmigo(){
-        return this.usuario.getListaAmigo();
-    }
-    
-    public void setNombres(String nombres){
-        this.usuario.setNombres(nombres);
-    }
-    
-    public void setApPaterno(String apPaterno){
-        this.usuario.setApPaterno(apPaterno);
-    }
-    
-    public void setApMaterno(String apMaterno){
-        this.usuario.setApMaterno(apMaterno);
-    }
-    
-    public void setCorreo(String correo){
-        this.usuario.setCorreo(correo);
-    }
-    
-    public void setPassword(String password){
-        this.usuario.setPassword(password);
-    }
-    
-    public void setFechaNacimiento(String fechaNacimiento){
-        this.usuario.setFechaNacimiento(fechaNacimiento);
-    }
-    
-    public void setSexo(char sexo){
-        this.usuario.setSexo(sexo);
-    }
-    
     
 }
